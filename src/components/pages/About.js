b1761baf96701d5c8873cc7f551c8e635982dd34
Gagan Singh <gagansingh.dev@outlook.com>
2020-06-28 17:57:17 -0400

    Initial Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+export const About = () => {
+   return (
+      <Fragment>
+         <h1>About This App</h1>
+         <p>Search Github Users</p>
+         <p>Version: 1.1.0</p>
+      </Fragment>
+   );
+};
